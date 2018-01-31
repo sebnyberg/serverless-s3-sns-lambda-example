dbf917d89f4ab32da24a47870dfa857c49fbee45
Sebastian Nyberg <seb.nyberg90@gmail.com>
2018-01-31 16:53:31 +0100

    js example done

--- src/handler.js
+++ src/handler.js
@@ -1,28 +1,12 @@
-import AWS from "aws-sdk";
-import debug from "debug";
+const AWS = require("aws-sdk");
 
-const fetchDataLogger = debug("app:lambda:fetchData");
-
-export const fetchData = async (event, context, callback) => {
-  fetchDataLogger("Context:");
-  fetchDataLogger(context);
-
-  fetchDataLogger("Target bucket name: ");
-  fetchDataLogger(process.env.TARGET_BUCKET_NAME);
-
-  fetchDataLogger("Number of records in event: %o", event.Records.length);
-
-  fetchDataLogger("-----------");
+export const fetchData = (event, context, callback) => {
   const message = JSON.parse(event.Records[0].Sns.Message);
-  fetchDataLogger(message);
 
-  fetchDataLogger("-----------");
-  fetchDataLogger("Number of records in message: %o", message.Records.length);
   const s3 = message.Records[0].s3;
-  fetchDataLogger(s3);
 
-  const arn = s3.bucket.arn;
-  const objectKey = s3.object.key;
+  const sourceBucketArn = s3.bucket.arn;
+  const sourceObjectKey = s3.object.key;
 
   callback(null, {
     statusCode: 200,
